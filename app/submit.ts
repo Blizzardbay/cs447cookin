3ff2f6cca17932a3b55938d3219fb9015bfcdaae
Blizzardbay <duttes@yahoo.com>
2024-10-14 15:27:41 -0400

    Update submit.ts

--- app/submit.ts
+++ app/submit.ts
@@ -4,7 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const data = req.body
-  const id = await createItem(data)
-  res.status(200).json({ id })
+  //const data = req.body
+  //const id = await createItem(data)
+  //res.status(200).json({ id })
 }
