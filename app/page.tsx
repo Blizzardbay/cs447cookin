872452da66a4778ffba0405274946405cec80206
Blizzardbay <duttes@yahoo.com>
2024-10-14 15:17:16 -0400

    Form Test Complete
    Completed creating the form input system in Next.js.

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,54 @@
+'use client';
 import { FormEvent } from 'react'
 
 export default function Home() {
-	async function onSubmit(event: FormEvent<HTMLFormElement>) {
+	async function userCreate(event: FormEvent<HTMLFormElement>) {
 		event.preventDefault()
 	 
 		const formData = new FormData(event.currentTarget)
-		const response = await fetch('/api/submit', {
+		const response = await fetch('/app/submit', {
 		  method: 'POST',
 		  body: formData,
 		})
-	 
+		
+		var temp_date = new Date()
+		
+		var user_table = {
+			"username" : formData.get("username"),
+			"password" : formData.get("password"),
+			"creation_date" : (temp_date.getMonth() + 1).toString() + "/" + temp_date.getDate().toString() + "/" + temp_date.getFullYear().toString() + " " + temp_date.getHours().toString() + ":" + temp_date.getMinutes().toString() + ":" + temp_date.getSeconds().toString(),
+			"associatedtable" : 0
+		}
+		
 		// Handle response if necessary
-		const data = await response.json()
-		console.log(data)
+		// const data = await response.json()
+		console.log(user_table)
+	}
+	async function userDestroy(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault()
+	 
+		const formData = new FormData(event.currentTarget)
+		const response = await fetch('/app/submit', {
+		  method: 'POST',
+		  body: formData,
+		})
+		
+		
 	}
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-		  <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-			<form onSubmit={onSubmit}>
-			  <input type="text" name="name" />
-			  <button type="submit">Submit</button>
+		<div className="">
+		  <main className="">
+			<form onSubmit={userCreate} style={{margin: "auto", textAlign: "Center"}}>
+			  <input type="text" name="username" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
+			  <br></br>
+			  <input type="text" name="password" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
+			  <br></br>
+			  <button type="createUser">Create User</button>
+			</form>
+			<form onSubmit={userDestroy} style={{margin: "auto", textAlign: "Center"}}>
+			  <input type="text" name="username" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
+			  <br></br>
+			  <button type="deleteUser">Delete User</button>
 			</form>
 		  </main>
 		  <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
