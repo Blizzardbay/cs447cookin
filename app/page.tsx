2c782bc7a3d383debd9cc2589a992ff7711ba140
JalenBrown36 <jbrown36@umbc.edu>
2024-11-06 15:21:44 -0500

    - Updated the page.tsx with UI for login - Updated tailwind.config.ts with a screen configuration - Created /util directory and moved data.tsx and create.tsx - Created /assets/images directory - Created /Component directory - Created /signup/page.tsx for displaying sign up page - Added next.config.js - Added fonts.ts into /fonts directory - Added 2 png images into /assests/images directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,67 @@
 'use client';
 
-import { insertUserData, removeUserData, tryUserLogin } from '@/app/data';
+import { insertUserData, removeUserData, tryUserLogin } from '@/app/util/data';
+import {inter, pacifico} from '@/app/fonts/fonts';
+import Image from 'next/image';
+import Link from 'next/link';
+import cookinIcon from '@/app/assets/images/cookinIcon1.png';
 
 export default function Home() {
 	return (
-		<div className="">
-		  <main className="">
-			<form action={insertUserData} style={{margin: "auto", textAlign: "center"}}>
+		<div className={`${inter.className} antialiased relative min-h-screen min-w-[768px] mx-auto flex flex-col gap-16 justify-center items-center`}>
+			<header className={`${pacifico.className} antialiased flex flex-col gap-4 items-center justify-center`}>
+				<h1 className='text-8xl'>Cookin'</h1>
+				<p className='text-2xl'>Cookin' Up Flavor, One Recipe at a Time!</p>
+			</header>
+		  	<main className="w-[30%] flex flex-col gap-6 justify-center items-center [&>form]:m-auto [&>form]:text-center [&>form]:w-full ">
+				<h3 className='text-4xl font-semibold'>Ready up 😋!</h3>
+			{/* <form action={insertUserData}>
+				<div className='flex flex-col items-start'>
+					<label htmlFor='username'>Email</label>
+					<input type="text" name="username" className="w-full h-8 border-[1px] border-black rounded-md"/>
+				</div>
+				<div className='flex flex-col items-start'>
+					<label htmlFor="password">Password</label>
+					<input type="text" name="password" className="w-full h-8 border-[1px] border-black rounded-md"/>
+				</div>
+				<div className='w-full text-white bg-black rounded-full'>
+					<button type="submit" className='h-8'>Create User</button>
+				</div>
+			</form>
+			<form action={removeUserData}>
 				<input type="text" name="username" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
 				<br></br>
-				<input type="text" name="password" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
-				<br></br>
-				<button type="submit">Create User</button>
-			</form>
-			<form action={removeUserData} style={{margin: "auto", textAlign: "center"}}>
-				<input type="text" name="username" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
-				<br></br>
-				<button type="submit">Delete User</button>
-			</form>
-			<form action={tryUserLogin} style={{margin: "auto", textAlign: "center"}}>
-				<input type="text" name="username" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
-				<br></br>
-				<input type="text" name="password" style={{borderStyle: "solid", borderWidth: "1px", borderColor: "black", marginBottom: "1px"}}/>
-				<br></br>
-				<button type="submit">Login</button>
-			</form>
+				<div className='w-full text-white bg-black rounded-full'>
+					<button type="submit" className='h-8'>Delete User</button>
+				</div>
+			</form> */}
+				<form action={tryUserLogin} className='w-full flex flex-col gap-6 [&>label]:font-semibold'>
+					<div className='flex flex-col gap-[2px] items-start'>
+						<label htmlFor='username' className='text-lg'>Email</label>
+						<input type="text" name="username" placeholder='Enter your email' className="w-full h-10 px-3 border-[1px] border-[#808080] rounded-md truncate"/>
+					</div>
+					<div className='flex flex-col gap-[2px] items-start'>
+						<label htmlFor="password" className='text-lg'>Password</label>
+						<input type="text" name="password" placeholder='Enter your password' className="w-full h-10 px-3 border-[1px] border-[#808080] rounded-md truncate"/>
+					</div>
+					<div className='w-full text-white bg-black rounded-full'>
+						<button type="submit" className='h-10 text-lg'>Login</button>
+					</div>
+				</form>
+				<p>Need an account? <Link href="/" className='font-semibold'>Sign up</Link></p>
+
 			</main>
+			<Image
+				src={cookinIcon}
+				alt='Cookin Icon'
+				className='absolute w-[512px] bottom-[25%] -right-[40%] lg:-right-[20%] xl:-right-[10%]'
+			/>
+			<Image
+				src={cookinIcon}
+				alt='Cookin Icon'
+				className='absolute w-[512px] bottom-[5%] -left-[40%] lg:-left-[20%] xl:-left-[10%] scale-x-[-1]'
+			/>
+
 			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 			</footer>
 		</div>
